b8d77f0618ff2be8607e7afacba3ccdef2cfaa0b
John Brown <digitaldream0719@gmail.com>
2025-03-17 22:29:14 -0400

    feat: initial port

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use environment variable or default to 3000
 
+app.get('/', (req, res) => {
+  res.send('Hello from Render!');
+});
 app.use(bodyParser.json()); // For parsing application/json
 app.use('/api', userRoutes); // Use user routes
 app.use('/api', marketRoutes);
